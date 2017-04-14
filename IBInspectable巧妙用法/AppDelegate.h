1025eca3d8e4f2958cb87a1f614b672346deef0b
mm <mm@mmdemac-mini.local>
2017-04-14 11:05:13 -0700

    Initial Commit

---
+++ IBInspectable巧妙用法/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBInspectable巧妙用法
+//
+//  Created by mm on 17/4/14.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
